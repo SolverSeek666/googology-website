03bd92dcf3f799e02e144fb6d815344d52d7e6aa
Seek <solverseek666@gmail.com>
2026-07-12 21:47:23 +0800

    Update tab.js
    fdgtrdt4dsfdsgdfgery

--- OCF index/tab.js
+++ OCF index/tab.js
@@ -1,1 +1,10 @@
-
+let btnTabs = document.getElementsByClassName("btntab");
+let tabs = document.getElementsByClassName("tab");
+let lastTab = -1;
+for (let i = 0; i < btnTabs.length; i++) {
+    btnTabs[i].onclick = function() {
+        tabs[i].style.display = "";
+        if (lastTab >= 0) tabs[lastTab].style.display = "none";
+        lastTab = i;
+    }
+}
