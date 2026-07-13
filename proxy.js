0656c836afae516ec2b5df7460cf8c26453c03b4
Seek <solverseek666@gmail.com>
2026-07-13 18:12:39 +0800

    Update proxy.js
    fghfgdstgdfs

--- proxy.js
+++ proxy.js
@@ -3,14 +3,14 @@ import { getDatabase, ref, set, onValue, onDisconnect } from "https://www.gstati
 import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1WnDm6XEtVNMFHFXDV9QCLVlJSo4-nrQ",
-  authDomain: "wy-sequence-lngi.firebaseapp.com",
-  databaseURL: "https://wy-sequence-lngi-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "wy-sequence-lngi",
-  storageBucket: "wy-sequence-lngi.firebasestorage.app",
-  messagingSenderId: "171132084225",
-  appId: "1:171132084225:web:f3be5025ff1dad79d62000",
-  measurementId: "G-TYVB29BNME"
+  apiKey: "AIzaSyCsBKRwzPo-Kn52o4m_AEzZK5zbu5NTx2M",
+  authDomain: "wy-lngi.firebaseapp.com",
+  databaseURL: "https://wy-lngi-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "wy-lngi",
+  storageBucket: "wy-lngi.firebasestorage.app",
+  messagingSenderId: "912532176065",
+  appId: "1:912532176065:web:262019e355d2dc1e18e840",
+  measurementId: "G-LDRNDTDVKG"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -28,7 +28,7 @@ onAuthStateChanged(auth, (user) => {
   const connectedRef = ref(db, ".info/connected");
 
   onValue(connectedRef, (snap) => {
-    if (snap.val()) {
+    if (snap.val() === true) {
       set(userRef, true);
       onDisconnect(userRef).remove();
     }
