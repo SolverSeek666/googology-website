1d9730d788e1a007907175822befe3aadd1d090e
Seek <solverseek666@gmail.com>
2026-07-28 19:11:48 +0800

    Update tab.js
    yes

--- OCF index/tab.js
+++ OCF index/tab.js
@@ -3,6 +3,7 @@ let tabs = document.getElementsByClassName("tab");
 let lastTab = -1;
 for (let i = 0; i < btnTabs.length; i++) {
     btnTabs[i].onclick = function() {
+        if (i == lastTab) return;
         tabs[i].style.display = "";
         if (lastTab >= 0) tabs[lastTab].style.display = "none";
         lastTab = i;
